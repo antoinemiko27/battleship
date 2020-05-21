dbbfec985c5fd34d8597d84e60a9f8d39f38c136
antoinemiko27 <61541517+antoinemiko27@users.noreply.github.com>
2020-05-21 08:22:13 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<head>
+  <title>Warship battle</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+
+
+
+<template>
+  <div id="app">
+
+    <a href="/jeu"><input type="button"  value="create a game "></a>
+
+
+
+
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
